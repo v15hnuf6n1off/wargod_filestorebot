8c3beae56fed2b1e93ad726a61ec140771371b30
v15hnuf6n1off <145260326+v15hnuf6n1off@users.noreply.github.com>
2023-10-07 17:35:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = ""
+USER_REPLY_TEXT = "Bot is alive till"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
