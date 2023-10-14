4bbeb1c7bc5cb7e5fdb2b32789e3f5db18d4e8f4
v15hnuf6n1off <145260326+v15hnuf6n1off@users.noreply.github.com>
2023-10-14 11:14:49 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>✑ ᴄʀᴇᴀᴛᴏʀ :<a href='tg://user?id={OWNER_ID}'ʏᴏᴜʀ ғʀɪᴇɴᴅ</a>\n✑ ʟᴀɴɢᴜᴀɢᴇ :<a href='https://python.org'>ᴘʏᴛʜᴏɴ</a>\n✑ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ ᴀsʏɴᴄɪᴏ</a>\n✑ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://t.me/v15hnuf6n1x'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n✑ ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/A1pher>ᗩ1ᑭᕼᗴᖇ</a>\n✑ ᴅᴇᴠ : <a href='https://t.me/Mr_V_bots'>Mr_V_bots</a></b>",
+            text = f"<b>✑ ᴄʀᴇᴀᴛᴏʀ :<a href='https://t.me/user?id={OWNER_ID}'ʏᴏᴜʀ ғʀɪᴇɴᴅ</a>\n✑ ʟᴀɴɢᴜᴀɢᴇ :<a ᴘʏᴛʜᴏɴ</a>\n✑ ʟɪʙʀᴀʀʏ : <a ᴘʏʀᴏɢʀᴀᴍ ᴀsʏɴᴄɪᴏ</a>\n✑ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://t.me/v15hnuf6n1x'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n✑ ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/A1pher>Tᗴᗩᗰ ᗩ1ᑭᕼᗴᖇ</a>\n✑ ᴅᴇᴠ : <a href='https://t.me/Mr_V_bots'>Mr_V_bots</a></b>",
             disable_web_page_preview = False,
             reply_markup = InlineKeyboardMarkup(
                 [
