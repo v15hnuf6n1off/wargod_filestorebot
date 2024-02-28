9931cfe0fbdcc901f5f36acdac66a528245eb767
v15hnuf6n1off <145260326+v15hnuf6n1off@users.noreply.github.com>
2024-02-28 12:06:50 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -23,7 +23,7 @@ async def is_subscribed(filter, client, update):
     if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
         return False
     try:
-        member = await client.get_chat_member(chat_id = FORCESUB_CHANNEL2, user_id = user_id)
+        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL2, user_id = user_id)
     except UserNotParticipant:
         return False
     else:
